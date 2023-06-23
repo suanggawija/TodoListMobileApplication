5e724858f1fc9dfc8028002573dbd3721a6a1cb6
suangga <suanggawija@gmail.com>
2023-06-23 14:39:39 +0800

    merubah urutan tampil mulai dari spash screen dan memperbaiki intent

--- app/src/main/java/com/example/todolistapplication/LoginActivity.java
+++ app/src/main/java/com/example/todolistapplication/LoginActivity.java
@@ -2,13 +2,25 @@ package com.example.todolistapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button login_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login_button = findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginButton = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(loginButton);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/todolistapplication/SplashActivity.java
+++ app/src/main/java/com/example/todolistapplication/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.todolistapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        View decorView = getWindow().getDecorView();
+
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
