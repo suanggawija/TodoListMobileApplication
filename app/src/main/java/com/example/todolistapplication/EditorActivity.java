0f633eba188307857f32d817fb50e599529884f4
suangga <suanggawija@gmail.com>
2023-06-23 14:13:16 +0800

    menambahkan Inten pada Main_Activity dan Editor Activity

--- app/src/main/java/com/example/todolistapplication/EditorActivity.java
+++ app/src/main/java/com/example/todolistapplication/EditorActivity.java
@@ -2,13 +2,33 @@ package com.example.todolistapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditorActivity extends AppCompatActivity {
-
+    Button save_button, kembali;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+        save_button = findViewById(R.id.save_button);
+        kembali = findViewById(R.id.kembali);
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent saveButton = new Intent(EditorActivity.this, MainActivity.class);
+                startActivity(saveButton);
+            }
+        });
+
+        kembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent kembaliButton = new Intent(EditorActivity.this, MainActivity.class);
+                startActivity(kembaliButton);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/todolistapplication/MainActivity.java
+++ app/src/main/java/com/example/todolistapplication/MainActivity.java
@@ -8,18 +8,27 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button add_button;
+    Button add_button, edit_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         add_button = findViewById(R.id.add_button);
+        edit_button = findViewById(R.id.edit);
 
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent addButton = new Intent(MainActivity.this, EditorActivity.class);
                 startActivity(addButton);
+            }
+        });
+
+        edit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent aditButton = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(aditButton);
             }
         });
     }
