4d51e92322fdc9e7c2e1fe9c0b4e69e417c0ccf2
suangga <suanggawija@gmail.com>
2023-06-23 13:43:18 +0800

    menambahkan editor_activity, splash_activity, dan logi_activity

--- app/src/main/java/com/example/todolistapplication/MainActivity.java
+++ app/src/main/java/com/example/todolistapplication/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.todolistapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button add_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add_button = findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addButton = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(addButton);
+            }
+        });
     }
 }
